55b34045bf59231a6a427aadc632f35a86044672
Artemzhi38 <artemzhi38@gmail.com>
2021-09-09 11:45:03 +0300

    fixed task5 to be one function

--- homeworks/homework2/task5.py
+++ homeworks/homework2/task5.py
@@ -14,33 +14,22 @@ assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) ==
 from typing import Any, Iterable, List
 
 
-def custom_range(*args, **kwargs) -> List[Any]:
-
-    def one_param(coll: Iterable, stop: Any) -> List[Any]:
-        """Function to use when only stop argument is stated"""
-        result = []
-        for element in coll:
-            if element != stop:
-                result.append(element)
-            else:
-                return result
-
-    def two_or_three_params(coll: Iterable, start: Any,
-                            stop: Any, step=1) -> List[Any]:
-        """Function to use when start and step argument are stated"""
-        result = []
-        collection = list(coll)
-        if step < 0:
-            collection.reverse()
-            step = abs(step)
-        index = collection.index(start)
-        while index < collection.index(stop):
-            result.append(collection[index])
-            index += step
-        return result
-
-    '''Here we decide which function to chose'''
-    if len(args)+len(kwargs) < 3:
-        return one_param(*args, **kwargs)
-    else:
-        return two_or_three_params(*args, **kwargs)
+def custom_range(coll: Iterable, start: Any,
+                 stop=None, step=1) -> List[Any]:
+    result = []
+    collection = list(coll)
+# checking if amount of input arguments was > 2
+# changing start and stop params otherwise
+    if stop is None:
+        start, stop = collection[0], start
+# checking if step argument is negative
+# reversing our collection if it is
+    if step < 0:
+        collection.reverse()
+        step = abs(step)
+# filling result list
+    index = collection.index(start)
+    while index < collection.index(stop):
+        result.append(collection[index])
+        index += step
+    return result
