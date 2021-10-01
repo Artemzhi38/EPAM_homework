42bec2b4599c28181ca2d8e2f227697d7aeb08f1
Artemzhi38 <artemzhi38@gmail.com>
2021-10-01 15:23:05 +0300

    fixed task2 tests

--- tests/test_homework5/test_task2.py
+++ tests/test_homework5/test_task2.py
@@ -1,48 +1,46 @@
-from homeworks.homework5.task2 import custom_sum
+from homeworks.homework5.task2 import custom_sum, print_result
 
 
 # Positive tests
-def test_decorated_function_works_with_two_params(capsys):
+def test_decorated_function_works_with_two_params():
     """Testing that decorated original function works
-    correctly with two arguments and give the result
-    two times(first - for 'print' inside decorator and
-    second - for 'return' in original function)"""
-    assert custom_sum([1, 2, 3], [4, 5]) == [1, 2, 3, 4, 5]
-    captured = capsys.readouterr()
-    assert captured.out == "[1, 2, 3, 4, 5]\n"
+    correctly with two arguments"""
+    original_custom_sum = custom_sum
+    decorated_custom_sum = print_result(custom_sum)
+    assert original_custom_sum([1, 2, 3], [4, 5]) == \
+           decorated_custom_sum([1, 2, 3], [4, 5])
 
 
-def test_decorated_function_works_with_four_params(capsys):
+def test_decorated_function_works_with_four_params():
     """Testing that decorated original function works
     correctly with four arguments"""
-    assert custom_sum(1, 2, 3, 4) == 10
-    captured = capsys.readouterr()
-    assert captured.out == "10\n"
+    original_custom_sum = custom_sum
+    decorated_custom_sum = print_result(custom_sum)
+    assert original_custom_sum(1, 2, 3, 4) == \
+           decorated_custom_sum(1, 2, 3, 4)
 
 
 def test_original_function_docstring_is_callable():
     """Testing that decorated original function docstring
     is accessible with 'function.__doc__' command"""
-    assert custom_sum.__doc__ == 'This function can sum any' \
-                                 ' objects which have __add___'
+    original_custom_sum = custom_sum
+    decorated_custom_sum = print_result(custom_sum)
+    assert original_custom_sum.__doc__ == \
+        decorated_custom_sum.__doc__
 
 
 def test_original_function_name_is_callable():
     """Testing that decorated original function name is
      accessible with 'function.__name__' command"""
-    assert custom_sum.__name__ == 'custom_sum'
+    original_custom_sum = custom_sum
+    decorated_custom_sum = print_result(custom_sum)
+    assert original_custom_sum.__name__ == \
+           decorated_custom_sum.__name__
 
 
 def test_original_function_is_callable():
-    """Testing that original function is accessible with
-     'function.__original_func' command"""
-    assert callable(custom_sum.__original_func)
-
-
-def test_called_original_function_works(capsys):
-    """Testing that original function accessed with
-    'function.__original_func' command does not print
-     any output(gives result only once)"""
-    assert custom_sum.__original_func(1, 2, 3, 4) == 10
-    captured = capsys.readouterr()
-    assert captured.out == ""
+    """Testing that original function is accessible from
+    decorated function with 'function.__original_func' command"""
+    original_custom_sum = custom_sum
+    decorated_custom_sum = print_result(custom_sum)
+    assert decorated_custom_sum.__original_func == original_custom_sum
